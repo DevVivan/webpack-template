04e3ae8c69d35235ee5b05941a50180eebd927cf
Vivan <vivanpanda13@gmail.com>
2023-07-25 21:39:04 +0400

    Load images using asset webpack management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './assets/styles/style.css'
+import LogoImage from './assets/img/thisisanimage.png';
 
 const element = document.createElement('div');
 element.innerHTML = 'Hello!';
 element.classList.add('hello');
 document.body.appendChild(element);
+
+const Image = new Image();
+Image.src = LogoImage;
+
+document.body.appendChild(Image);
